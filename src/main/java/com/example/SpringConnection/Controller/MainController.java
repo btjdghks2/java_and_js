0069350e36ef73bda531e863556d21cffb09d486
btjdghks2 <btjdghks2@gmail.com>
2023-08-16 18:50:10 +0900

    main findAll

--- src/main/java/com/example/SpringConnection/Controller/MainController.java
+++ src/main/java/com/example/SpringConnection/Controller/MainController.java
@@ -2,26 +2,34 @@ package com.example.SpringConnection.Controller;
 
 
 import com.example.SpringConnection.Dto.MainPageProductListDto;
+import com.example.SpringConnection.Repository.ProductRepository;
 import com.example.SpringConnection.Service.MainService;
 import com.example.SpringConnection.domain.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MainController {
 
     private final MainService mainService;
+    private final ProductRepository productRepository;
 
 
-    public ResponseEntity<List<MainPageProductListDto>> MainPageProductListControl(MainPageProductListDto mainPageProductListDto) {
+    @GetMapping("/api/main")
+    public List<MainPageProductListDto> MainPageProductListControl() {
 
-        Product product = new Product();
-        product = mainService.MainPageProductListService(mainPageProductListDto);
-        return
+        List<Product> products = productRepository.findAll();
+        List<MainPageProductListDto> result = products.stream()
+                .map(p -> new MainPageProductListDto(p))
+                .collect(Collectors.toList());
+        return result;
+
 
     }
 }
